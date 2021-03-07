c39be2aeffa3d7efcb751ddbc26f478bf562529a
Ely Hess <Ely.hess@me.com>
2021-03-07 16:00:55 -0700

    Add forcast index action

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::ForecastController < ApplicationController
 	def index
-
+		if params[:location].present?
+			forecast = ForecastFacade.fetch_forecast(params[:location])
+			render json: ForecastSerializer.new(forecast)
+		else
+			render json: { error: "Location missing or incorrectly entered."}
+		end
 	end
 end
