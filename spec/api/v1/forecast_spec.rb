6f8dd13e441938d035976fe195ea9873b5b472e4
Ely Hess <Ely.hess@me.com>
2021-03-07 09:57:14 -0700

    Add forecast controller & spec

---
+++ spec/api/v1/forecast_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe "Forecast API" do
+	describe "Happy path" do
+		it "gets weather for a location" do
+			
+		end
+	end
+
+	describe "Sad Path" do
+		it 'should ' do
+			
+		end
+	end
+end
