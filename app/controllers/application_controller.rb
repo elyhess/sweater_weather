78feedcb2876c6da27edc329349241cf40e6f508
Ely Hess <Ely.hess@me.com>
2021-03-07 16:01:36 -0700

    Add 503 service unavailable rescue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+	rescue_from JSON::ParserError, with: :json_render_failure
+
+	def json_render_failure
+		render json: { error: "API currently unavailable. Hold tight, we're working on it!"}, status: 503
+	end
+
 end
