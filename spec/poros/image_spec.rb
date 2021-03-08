3f41a342a8b7fbc771ef4e23134ff08ae0ee16ae
Ely Hess <Ely.hess@me.com>
2021-03-07 18:33:29 -0700

    Add image poro spec

---
+++ spec/poros/image_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Image, :vcr do
+	it 'exists and has attributes' do
+		location = "Denver,co"
+		image_data = ImageService.random_image(location).first
+
+		image = Image.new(image_data, "Denver,co")
+
+		expect(image).to be_an_instance_of(Image)
+		expect(image.image).to be_a Hash
+		expect(image.image[:location]).to eq(location)
+		expect(image.image[:urls].count).to eq(5)
+		image.image[:urls].values.each do |value|
+			expect(value).to be_a String
+		end
+		expect(image.credit).to be_a Hash
+		expect(image.credit.keys.count).to eq(3)
+		expect(image.credit[:profile_img]).to be_a String
+		expect(image.credit[:author]).to eq(image_data[:user][:username])
+		expect(image.credit[:links]).to eq(image_data[:user][:links])
+	end
+end
