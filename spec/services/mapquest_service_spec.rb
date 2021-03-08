91f38adabf86b1f86a75f7eb9301bb79f46d8f05
Ely Hess <Ely.hess@me.com>
2021-03-08 09:51:18 -0700

    Add get_estimated_travel_time service test

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -16,5 +16,16 @@ describe MapquestService, :vcr do
 			expect(data[:results][0][:locations][0][:latLng]).to have_key :lng
 			expect(data[:results][0][:locations][0][:latLng][:lng]).to be_a(Numeric)
 		end
+
+		it '::get_estimated_travel_time' do
+			data = MapquestService.get_estimated_travel_time("Boulder,co", "Denver,co")
+
+			expect(data).to be_a Hash
+
+			expect(data.keys).to eq([:route, :info])
+			expect(data[:route].size).to eq(23)
+			expect(data[:route][:distance]).to be_a(Float)
+			expect(data[:route][:formattedTime]).to be_a String
+		end
 	end
 end
