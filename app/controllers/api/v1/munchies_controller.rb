77a6cc114a0c43d33861520c882fa839d01642d1
Ely Hess <Ely.hess@me.com>
2021-03-08 11:24:26 -0700

    Add munchies serializer & facade to index action

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,4 +1,10 @@
 class Api::V1::MunchiesController < ApplicationController
 	def index
+		if params[:start].present? && params[:destination].present? && params[:food].present?
+			munchie = MunchieFacade.create_food_itinerary(params[:start], params[:destination], params[:food])
+			render json: MunchieSerializer.new(munchie)
+		else
+			render json: { error: "Invalid parameters - please refer to documentation for the correct request format."}
+		end
 	end
 end
