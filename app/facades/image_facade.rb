60790350e2dbb3f7cebefc0697c49607e8e59bd0
Ely Hess <Ely.hess@me.com>
2021-03-07 18:33:01 -0700

    Add image facade

--- app/facades/image_facade.rb
+++ app/facades/image_facade.rb
@@ -1,8 +1,8 @@
-class BackgroundsFacade
+class ImageFacade
 	class << self
 		def fetch_background(location)
-			image_data = BackgroundsService.random_image(location).first
-			Background.new(image_data, location)
+			image_data = ImageService.random_image(location).first
+			Image.new(image_data, location)
 		end
 	end
 end
