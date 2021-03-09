a49a70a078d9d7401104ca0ce09b2e149c536c43
Ely Hess <Ely.hess@me.com>
2021-03-09 15:36:01 -0700

    Add .present?

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::SessionsController < ApplicationController
 
 	def create
 		user = User.find_by(email: params[:email])
-		if user && user.authenticate(params[:password])
+		if user.present? && user.authenticate(params[:password])
 			render json: UsersSerializer.new(user)
 		else
 			render json: { error: "Incorrect credentials." }, status: :bad_request
