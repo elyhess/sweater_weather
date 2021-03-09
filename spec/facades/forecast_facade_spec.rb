593be63a3a029ce45c21ad0112a8e744fdc9e649
Ely Hess <Ely.hess@me.com>
2021-03-09 15:19:00 -0700

    Add weather_at_eta spec

--- spec/facades/forecast_facade_spec.rb
+++ spec/facades/forecast_facade_spec.rb
@@ -40,5 +40,28 @@ describe ForecastFacade, :vcr do
 			expect(ForecastFacade.daily_weather(forecast)).to be_an(Array)
 			expect(ForecastFacade.daily_weather(forecast).first).to be_an_instance_of(DailyWeather)
 		end
+
+		it '::weather_at_eta' do
+			travel_time = { formatted_time: "00:34:12",
+			                unix_time: 123123 }
+			forecast = ForecastFacade.weather_at_eta("Boulder,co", travel_time)
+
+			expect(forecast).to be_a Hash
+			expect(forecast).to have_key(:dt)
+			expect(forecast).to have_key(:temp)
+			expect(forecast).to have_key(:feels_like)
+			expect(forecast).to have_key(:pressure)
+			expect(forecast).to have_key(:humidity)
+			expect(forecast).to have_key(:dew_point)
+			expect(forecast).to have_key(:uvi)
+			expect(forecast).to have_key(:clouds)
+			expect(forecast).to have_key(:visibility)
+			expect(forecast).to have_key(:wind_speed)
+			expect(forecast).to have_key(:wind_deg)
+			expect(forecast).to have_key(:weather)
+			expect(forecast[:weather][0]).to be_a(Hash)
+			expect(forecast[:weather][0].keys).to eq([:id, :main, :description, :icon])
+			expect(forecast).to have_key(:pop)
+		end
 	end
 end
