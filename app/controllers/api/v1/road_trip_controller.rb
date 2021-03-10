40249d37c3c859d8c337bab4d5a197470eeb3776
Ely Hess <Ely.hess@me.com>
2021-03-09 18:03:39 -0700

    Add RecordNotFound rescue

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -2,16 +2,12 @@ class Api::V1::RoadTripController < ApplicationController
 	before_action :reject_query_params, only: [:create]
 
 	def create
-		user = User.find_by(api_key: trip_params[:api_key])
-		if user.present?
-			if user.present? && params[:origin].present? && params[:destination].present?
-				road_trip = RoadTripFacade.create_road_trip(params[:origin], params[:destination])
-				render json: RoadtripSerializer.new(road_trip), status: :created
-			else
-				render json: { error: "Origin & destination must both be present in request." }, status: :bad_request
-			end
+		user = User.find_by!(api_key: trip_params[:api_key])
+		if user.present? && params[:origin].present? && params[:destination].present?
+			road_trip = RoadTripFacade.create_road_trip(params[:origin], params[:destination])
+			render json: RoadtripSerializer.new(road_trip), status: :created
 		else
-			render json: { error: "Invalid API key." }, status: :unauthorized
+			render json: { error: "Origin & destination must both be present in request." }, status: :bad_request
 		end
 	end
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::API
 	rescue_from JSON::ParserError, with: :json_render_failure
 	rescue_from ArgumentError, with: :bad_coordinates
+	rescue_from ActiveRecord::RecordNotFound, with: :invalid_api_key
 
 	def json_render_failure
 		render json: { error: "API currently unavailable. Hold tight, we're working on it!" }, status: :service_unavailable
@@ -14,6 +15,10 @@ class ApplicationController < ActionController::API
 		unless request.query_parameters.empty?
 			render json: { error: "Data must be sent in request body." }, status: :bad_request
 		end
+	end
+
+	def invalid_api_key
+		render json: { error: "Invalid API key." }, status: :unauthorized
 	end
 
 end
